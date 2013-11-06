b32ade9359e24020717dc6a2c85ddd33892b6b16
hamaco <hamanaka.kazuhiro@gmail.com>
2013-11-07 01:26:18 +0900

    Add users cookbook

---
+++ recipes/data_bag.rb
@@ -0,0 +1,31 @@
+data_ids = data_bag("users")
+data_ids.each do |data_id|
+  usr = data_bag_item("users", data_id)
+
+  user usr["id"] do
+    uid usr["uid"]
+    shell usr["shell"]
+    password usr["password"]
+    comment usr["comment"]
+    supports :manage_home => true
+    action :create
+  end
+
+  if usr["ssh_keys"]
+    directory "#{usr["home"]}/.ssh" do
+      owner usr["id"]
+      group usr["id"]
+      mode 0700
+      action :create
+    end
+
+    file "#{usr["home"]}/.ssh/authorized_keys" do
+      # source "authorized_keys.erb"
+      owner usr["id"]
+      group usr["id"]
+      mode 0600
+      content usr["ssh_keys"].join("\n")
+      action :create
+    end
+  end
+end

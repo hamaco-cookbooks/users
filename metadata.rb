bb4af0c6b9b3cd1190c412b7259b74e9afd7a052
hamaco <hamanaka.kazuhiro@gmail.com>
2013-11-07 10:25:48 +0900

    add metadata.rb & README

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             "users"
+maintainer       "hamaco"
+maintainer_email "hamanaka.kazuhiro@gmail.com"
+license          "MIT"
+description      "Installs/Configures users"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version          "0.0.1"

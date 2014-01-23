8df2955bee3eede14031b71ba94faa00d6ee90a9
hamaco <hamanaka.kazuhiro@gmail.com>
2014-01-23 12:41:03 +0900

    cosmetic change

--- recipes/data_bag.rb
+++ recipes/data_bag.rb
@@ -3,29 +3,28 @@ data_ids.each do |data_id|
   usr = data_bag_item("users", data_id)
 
   user usr["id"] do
-    uid usr["uid"]
-    shell usr["shell"]
+    uid      usr["uid"]
     password usr["password"]
-    comment usr["comment"]
+    comment  usr["comment"]
+    shell    usr["shell"]
     supports :manage_home => true
-    action :create
+    action   :create
   end
 
   if usr["ssh_keys"]
     directory "#{usr["home"]}/.ssh" do
-      owner usr["id"]
-      group usr["id"]
-      mode 0700
+      owner  usr["id"]
+      group  usr["id"]
+      mode   "0700"
       action :create
     end
 
     file "#{usr["home"]}/.ssh/authorized_keys" do
-      # source "authorized_keys.erb"
-      owner usr["id"]
-      group usr["id"]
-      mode 0600
+      owner   usr["id"]
+      group   usr["id"]
+      mode    "0600"
       content usr["ssh_keys"].join("\n")
-      action :create
+      action  :create
     end
   end
 end
